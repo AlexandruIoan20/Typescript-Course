b84ea581dd24e3e5c1b41e976e1239aa96ae8990
AlexandruIoan20 <117534646+AlexandruIoan20@users.noreply.github.com>
2023-07-07 14:02:59 +0300

    Create todo

--- tsx/src/App.tsx
+++ tsx/src/App.tsx
@@ -1,12 +1,35 @@
-import './App.css'; 
+import React, { useState } from 'react';
 import InputField from './components/InputField';
+import { Todo } from './todo';
+import TodoList from './components/TodoList';
 
 const App: React.FC = () => { 
+  const [ todo, setTodo] = useState<string>(""); // useState <type> - merge si union <string | number > 
+  const [ todos, setTodos ] = useState<Todo[]>([]); 
+
+  const handleAdd = (e: React.FormEvent) => { 
+    e.preventDefault(); 
+
+    if(todo) { 
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]); 
+      setTodo(""); 
+    }
+  }
+
   return ( 
     <div className = 'app'>
       <span className = 'heading'> Taskify </span>
 
-      <InputField /> 
+      <InputField todo = { todo } setTodo = { setTodo } handleAdd = { handleAdd } /> 
+      <TodoList />
+
+      <ul>
+        { todos.map(t => { 
+          return ( 
+            <li>{ t.todo } </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
--- tsx/src/components/InputField.tsx
+++ tsx/src/components/InputField.tsx
@@ -1,7 +1,20 @@
-const InputField = () => {
+import React from "react"
+
+interface Props {
+  todo: string, 
+  setTodo: React.Dispatch<React.SetStateAction<string>> 
+  handleAdd: (e: React.FormEvent) => void, 
+}
+
+const InputField: React.FC<Props>  = ({ todo, setTodo, handleAdd }: Props) => {
   return (
-    <form className = "input">
-        <input type="input" placeholder = '...enter a task' className = 'input__box' />
+    <form className = "input" onSubmit = { (e) => { handleAdd(e) }  }>
+        <input type="text"
+          value = { todo }
+          placeholder = '...enter a task'
+          onChange = { (e) => setTodo(e.target.value)}
+          className = 'input__box'
+        />
         <button className = 'input__submit' type = 'submit'> GO </button>
     </form>
   )
